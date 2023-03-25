0a7e906ac64dba7cf34059118160b947f1b14751
bagnabendu <nabendubag8942@gmail.com>
2023-03-26 00:52:01 +0530

    blog add complete

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -1,6 +1,7 @@
 const router=require('express').Router();
 const AdminController=require("../controllers/AdminController");
 const DoctorController = require('../controllers/DoctorController')
+const BlogController = require('../controllers/BlogController')
 
 
 
@@ -9,11 +10,15 @@ router.post('/sigin', AdminController.admin_login);
 
 router.get('/dashboard',AdminController.adminAuth, AdminController.dashboard);
 router.get('/users',AdminController.adminAuth, AdminController.user);
-router.get('/blog',AdminController.adminAuth, AdminController.blog);
 router.get('/logout', AdminController.logout)
 
 // Doctor Router
 router.get('/doctor',AdminController.adminAuth, DoctorController.doctor);
 router.post('/adddoctor' , DoctorController.addDoctor)
+
+//Blog Router
+router.get('/blog',AdminController.adminAuth, BlogController.blog);
+router.post('/addblog' , BlogController.addBlog)
+
 
 module.exports=router;
